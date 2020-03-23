01b6b29a8eac07384029c85994f816e9ed10f293
chuanchic <chuanchic@163.com>
2020-03-23 09:00:57 +0800

    提交代码

--- app/src/main/java/com/test/androidstudynotes/MainActivity.java
+++ app/src/main/java/com/test/androidstudynotes/MainActivity.java
@@ -1,13 +1,31 @@
 package com.test.androidstudynotes;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.test.androidstudynotes.eventbus.EventbusActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private MainActivity mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
+
+        TextView tv_eventbus_click = findViewById(R.id.tv_eventbus_click);
+        tv_eventbus_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(mContext, EventbusActivity.class));
+            }
+        });
+
+
     }
+
 }
---
+++ app/src/main/java/com/test/androidstudynotes/eventbus/EventbusActivity.java
@@ -0,0 +1,17 @@
+package com.test.androidstudynotes.eventbus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.test.androidstudynotes.R;
+
+public class EventbusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventbus);
+
+    }
+
+}
